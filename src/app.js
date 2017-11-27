d33ac15e132edb76de9fd9dcc49a59d2c3b9342f
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-26 18:02:45 -0800

    Add error handling around our API

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const BOOK_FIELDS = ['title', 'author', 'publication_year'];
 // Starts undefined - we'll set this in $(document).ready
 // once we know the template is available
 let bookTemplate;
+let statusMessageTemplate;
 
 const readForm = function readForm() {
   const bookData = {};
@@ -49,8 +50,44 @@ const render = function render(bookList) {
   $(`th.sort.${ bookList.comparator }`).addClass('current-sort-field');
 };
 
+const reportStatus = function reportStatus(status, message) {
+  console.log(`Reporting ${ status } message: ${ message }`);
+  const generatedHTML = statusMessageTemplate({
+    status: status,
+    message: message,
+  });
+
+  $('#status-messages ul').append(generatedHTML);
+  $('#status-messages').show();
+}
+
+const booksApiErrorHandler = function(model, response) {
+  // Step 1, remove this book from our list (we added it earlier)
+  // .destroy will also remove it from all collections
+  model.destroy();
+
+  // Step 2, report errors to the user
+  if (response.responseJSON['errors']) {
+    // Report errors from the server to the user
+    const errors = response.responseJSON['errors'];
+    for (const field in errors) {
+      for (const problem of errors[field]) {
+        reportStatus('error', `${ field }: ${ problem }`);
+      }
+    }
+
+  } else {
+    // Server did not give us errors - report a generic
+    // failure and log the full response
+    reportStatus('error', 'Could not save book');
+    console.log('Got error response from server:');
+    console.log(response);
+  }
+}
+
 $(document).ready(() => {
   bookTemplate = _.template($('#book-template').html());
+  statusMessageTemplate = _.template($('#status-message-template').html());
 
   // Build the collection from seed data
   const bookList = new BookList();
@@ -69,9 +106,11 @@ $(document).ready(() => {
 
     const bookData = readForm();
     const book = bookList.add(bookData);
-    book.save();
+    book.save({}, {
+      success: clearForm,
+      error: booksApiErrorHandler
+    });
 
-    clearForm();
   });
 
   // Build event handlers for each of the table headers
@@ -83,4 +122,10 @@ $(document).ready(() => {
       bookList.sort();
     });
   });
+
+  // Clear status messages when the user clicks the button
+  $('#status-messages button.clear').on('click', (event) => {
+    $('#status-messages ul').html('');
+    $('#status-messages').hide();
+  })
 });
